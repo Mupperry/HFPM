1d346f8f0aaa528ee2b29564743c517a1205addd
Victor Hanson <vectoralarm@gmail.com>
2019-09-01 18:52:07 +0200

    Comments and ReadMe

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+# Leave as 127.0.0.1 if the broker runs on the same system as the power monitor.
 mqttBroker = "127.0.0.1"
+
 mqttTopic = "topic/to/publish/to"
+
+# What pin to listen for pulses on (according to the "Board" pin numbering scheme)
 pulsePin = 11
+
+# How many pulses the power meter produces per kWh used (normally written on power meter, e.g. imp/kWh)
 pulsesPerKWh = 100
+
+# Needed if using Thingsboard. Called "username" in MQTT-speak
 accessToken = ""
